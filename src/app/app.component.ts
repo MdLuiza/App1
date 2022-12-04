b5f5b1994f25dd9d0fa40558f2f8f1ad7719df8e
Zalaru Luiza <luizazalaru@gmail.com>
2022-12-04 21:41:41 +0200

    Second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'App1';
+  plans: any[] = [
+    {
+      type: "MONTHLY",
+      price: "$14.95",
+      
+    },
+    {
+      type: "3-MONTHLY",
+      price: "$38.00"
+    },
+    {
+      type: "ANUALLY",
+      price: "$134.00"
+
+    }
+  ]
+
+  constructor() {}
 }
